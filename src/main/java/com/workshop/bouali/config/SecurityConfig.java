ed4dffdb65b3eadc33f0864d5c8426094fdaf3d5
Ahmed <ahmed.405xy@gmail.com>
2026-05-25 19:52:59 +0300

    add security config and secure end points change BcryptPassword Encoder with Argon2id

--- src/main/java/com/workshop/bouali/config/SecurityConfig.java
+++ src/main/java/com/workshop/bouali/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.workshop.bouali.config;
 
+import com.workshop.bouali.config.filter.RateLimitingFilter;
 import com.workshop.bouali.repositories.employeerepo.EmployeeRepository;
 import com.workshop.bouali.users.SecurityUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,9 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +24,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final EmployeeRepository employeeRepository;
+    private final RateLimitingFilter rateLimitingFilter;
+
+
+    @Value("${app.security.crypto.argon2.parallelism:1}")
+    private int parallelism;
+
+    @Value("${app.security.crypto.argon2.memory:16384}")
+    private int memory;
+
+    @Value("${app.security.crypto.argon2.iterations:2}")
+    int iterations;
 
     @Bean
     public UserDetailsService userDetailsService(){
@@ -35,13 +49,14 @@ public class SecurityConfig {
     ) {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(rateLimitingFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(
                 authorize -> authorize
                         .requestMatchers("/api/address/**").hasAuthority("ROLE_ADMIN")
                         .requestMatchers("/api/employees/**").hasAuthority("ROLE_ADMIN")
 
                         .requestMatchers("/api/department/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
-                        .requestMatchers("/api/mission/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+                        .requestMatchers("/api/missions/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
                         .anyRequest().authenticated()
         )
                 .headers(Customizer.withDefaults())
@@ -52,6 +67,12 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new Argon2PasswordEncoder(
+                16,
+                32,
+                parallelism,
+                memory,
+                iterations
+        );
     }
 }
